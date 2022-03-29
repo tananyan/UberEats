944cb0d79641ff042a052c74c862035ebe1e612b
Boris <boris242008@ya.ru>
2022-03-29 15:35:05 +0300

    commit 2blocks

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,106 +1,51 @@
-const burger = document.querySelector(".burger"),
-  menu = document.querySelector(".menu"),
-  menuLink = document.querySelectorAll(".menu__ul"),
-  exit = document.querySelector(".menu__exit"),
-  overl = document.querySelector(".menu__overlay");
+// document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", function (event) {
+  const lang = document.querySelector(".footer__lang");
+  const langSelect = document.querySelector(".footer__lang-select");
+  // a.classList.add("b");
 
-// Кнопка меню
-burger.addEventListener("click", () => {
-  menu.classList.add("active");
-});
-exit.addEventListener("click", () => {
-  menu.classList.remove("active");
-});
-overl.addEventListener("click", () => {
-  menu.classList.remove("active");
-});
-menuLink.forEach((item) => {
-  item.addEventListener("click", () => {
-    menu.classList.remove("active");
+  lang.addEventListener("click", () => {
+    langSelect.style.display = "block";
+  });
+  langSelect.addEventListener("click", () => {
+    langSelect.style.display = "none";
   });
 });
 
-// Линия с процентам и
-const counters = document.querySelectorAll(".skills__footer-count"),
-  linesSpan = document.querySelectorAll(".skills__footer-line span");
+// const burger = document.querySelector(".burger"),
+//   menu = document.querySelector(".menu"),
+//   menuLink = document.querySelectorAll(".menu__ul"),
+//   exit = document.querySelector(".menu__exit"),
+//   overl = document.querySelector(".menu__overlay");
 
-counters.forEach((item, index) => {
-  linesSpan[index].style.width = item.innerHTML;
-});
+// // Кнопка меню
+// burger.addEventListener("click", () => {
+//   menu.classList.add("active");
+// });
+// exit.addEventListener("click", () => {
+//   menu.classList.remove("active");
+// });
+// overl.addEventListener("click", () => {
+//   menu.classList.remove("active");
+// });
+// menuLink.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     menu.classList.remove("active");
+//   });
+// });
 
-// Валидация формы
-function formValidation(form) {
-  $(form).validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 2,
-        maxlength: 25,
-      },
-      email: {
-        required: true,
-        email: true,
-      },
-      text: {
-        required: true,
-        minlength: 5,
-        maxlength: 800,
-      },
-      policy: {
-        required: true,
-      },
-    },
-    messages: {
-      name: {
-        required: "Пожалуйста, введите ваше имя",
-        minlength: jQuery.validator.format("Минимальная длина {0} букв"),
-        maxlength: jQuery.validator.format("Максимальная длина {0} букв"),
-      },
-      email: {
-        required: "Пожалуйста, введите e-mail",
-        email: "Пример почты name@pochta.ru",
-      },
-      text: {
-        required: "Пожалуйста, введите сообщение",
-        minlength: jQuery.validator.format("Минимальная длина {0} символов"),
-        maxlength: jQuery.validator.format("Максимальная длина {0} символов"),
-      },
-      policy: {
-        required: "Это обязательное условие",
-      },
-    },
-  });
-}
-formValidation("#contact-form");
+// counters.forEach((item, index) => {
+//   linesSpan[index].style.width = item.innerHTML;
+// });
 
-// Скроллинг вверх и вниз
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 2700) {
-    $(".scroll-up").fadeIn("fast");
-    $(".scroll-down").fadeOut("fast");
-  } else {
-    $(".scroll-up").fadeOut("fast");
-    $(".scroll-down").fadeIn("fast");
-  }
-});
-
-// Почта mailer
-$("form").submit(function (e) {
-  e.preventDefault();
-  if (!$(this).valid()) {
-    return;
-  }
-  $.ajax({
-    type: "POST",
-    url: "mailer/smart.php",
-    data: $(this).serialize(),
-  }).done(function () {
-    $(this).find("input").val("");
-    $(".form-thanks").fadeIn("fast");
-
-    $("form").trigger("reset");
-
-    $(".form-thanks").delay(3000).fadeOut("slow");
-  });
-  return false;
-});
+// // Скроллинг вверх и вниз
+// $(window).scroll(function () {
+//   if ($(this).scrollTop() > 2700) {
+//     $(".scroll-up").fadeIn("fast");
+//     $(".scroll-down").fadeOut("fast");
+//   } else {
+//     $(".scroll-up").fadeOut("fast");
+//     $(".scroll-down").fadeIn("fast");
+//   }
+// });
+// });
