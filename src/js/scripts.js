5e778ce3c408e3d1292c7883e2bcee7f4dde755d
Boris <boris242008@ya.ru>
2022-03-30 14:25:11 +0300

    scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,8 @@
 // document.addEventListener("DOMContentLoaded", () => {});
-document.addEventListener("DOMContentLoaded", function (event) {
-  const lang = document.querySelector(".footer__lang");
-  const langSelect = document.querySelector(".footer__lang-select");
-  // a.classList.add("b");
+document.addEventListener("DOMContentLoaded", function () {
+  // lang
+  const lang = document.querySelector(".footer__lang"),
+    langSelect = document.querySelector(".footer__lang-select");
 
   lang.addEventListener("click", () => {
     langSelect.style.display = "block";
@@ -10,42 +10,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
   langSelect.addEventListener("click", () => {
     langSelect.style.display = "none";
   });
+
+  // scroll
+  const scrollUp = document.querySelector(".scroll-up");
+
+  function myFunction() {
+    if (
+      document.body.scrollTop > 870 ||
+      document.documentElement.scrollTop > 870
+    ) {
+      scrollUp.style.display = "block";
+    } else {
+      scrollUp.style.display = "none";
+    }
+  }
+  window.onscroll = function () {
+    myFunction();
+  };
+
+  //
 });
-
-// const burger = document.querySelector(".burger"),
-//   menu = document.querySelector(".menu"),
-//   menuLink = document.querySelectorAll(".menu__ul"),
-//   exit = document.querySelector(".menu__exit"),
-//   overl = document.querySelector(".menu__overlay");
-
-// // Кнопка меню
-// burger.addEventListener("click", () => {
-//   menu.classList.add("active");
-// });
-// exit.addEventListener("click", () => {
-//   menu.classList.remove("active");
-// });
-// overl.addEventListener("click", () => {
-//   menu.classList.remove("active");
-// });
-// menuLink.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     menu.classList.remove("active");
-//   });
-// });
-
-// counters.forEach((item, index) => {
-//   linesSpan[index].style.width = item.innerHTML;
-// });
-
-// // Скроллинг вверх и вниз
-// $(window).scroll(function () {
-//   if ($(this).scrollTop() > 2700) {
-//     $(".scroll-up").fadeIn("fast");
-//     $(".scroll-down").fadeOut("fast");
-//   } else {
-//     $(".scroll-up").fadeOut("fast");
-//     $(".scroll-down").fadeIn("fast");
-//   }
-// });
-// });
